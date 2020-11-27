cae8879e8eba92c408f43afa4fc441ee8fbce29d
AnishAnand268 <73229170+AnishAnand268@users.noreply.github.com>
2020-11-27 20:47:39 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,17 +1,17 @@
 class Bob{
     constructor(x,y,radius){
      var options={
-       isStatic:true,
-       restitution:0.3,
-       friction:2.0,
-       density:1.2
+       isStatic:false,
+       restitution:1,
+       friction:0,
+       density:0.8
        
    }   
    
    this.x = x;
    this.y = y;
    this.radius = radius;
-   this.body = Bodies.circle(this.x ,this.y ,this.radius ,options)
+   this.body = Bodies.circle(this.x ,this.y ,(this.radius)/2 ,options)
    
   
    World.add(world,this.body);
